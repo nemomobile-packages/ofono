7034f70c7c3780a00b2c80c97f518020093ae637
Jussi Kangas <jussi.kangas@oss.tieto.com>
2013-10-25 16:03:11 +0300

    Make LTE configuration forcing working both ways
    Without this change phone will not change state corresponding
    to any in modem when enabling lte before user selects any.
    
    Signed-off-by: Jussi Kangas <jussi.kangas@oss.tieto.com>

--- ofono/drivers/rilmodem/radio-settings.c
+++ ofono/drivers/rilmodem/radio-settings.c
@@ -102,15 +102,31 @@ static void ril_set_rat_mode(struct ofono_radio_settings *rs,
 	}
 }
 
+static void ril_force_rat_mode(struct radio_data *rd, int pref)
+{
+	struct parcel rilp;
+
+	if (pref == rd->ratmode)
+		return;
+
+	parcel_init(&rilp);
+	parcel_w_int32(&rilp, 1);
+	parcel_w_int32(&rilp, rd->ratmode);
+	g_ril_send(rd->ril,
+		RIL_REQUEST_SET_PREFERRED_NETWORK_TYPE,
+		rilp.data, rilp.size, NULL,
+		NULL, g_free);
+	parcel_free(&rilp);
+}
+
 static void ril_rat_mode_cb(struct ril_msg *message, gpointer user_data)
 {
 	DBG("");
 	struct cb_data *cbd = user_data;
 	ofono_radio_settings_rat_mode_query_cb_t cb = cbd->cb;
-	struct parcel rilp, rilp_out;
+	struct parcel rilp;
 	int mode = OFONO_RADIO_ACCESS_MODE_ANY;
 	int pref;
-	struct radio_data *rd = NULL;
 
 	if (message->error == RIL_E_SUCCESS) {
 		ril_util_init_parcel(message, &rilp);
@@ -124,25 +140,18 @@ static void ril_rat_mode_cb(struct ril_msg *message, gpointer user_data)
 		case PREF_NET_TYPE_GSM_ONLY:
 			mode = OFONO_RADIO_ACCESS_MODE_GSM;
 			break;
+		case PREF_NET_TYPE_GSM_WCDMA_AUTO:/* according to UI design */
+			if (!cb)
+				ril_force_rat_mode(cbd->user, pref);
 		case PREF_NET_TYPE_WCDMA:
 		case PREF_NET_TYPE_GSM_WCDMA: /* according to UI design */
-		case PREF_NET_TYPE_GSM_WCDMA_AUTO:/* according to UI design */
 			mode = OFONO_RADIO_ACCESS_MODE_UMTS;
 			break;
 		case PREF_NET_TYPE_LTE_CDMA_EVDO:
 		case PREF_NET_TYPE_LTE_GSM_WCDMA:
 		case PREF_NET_TYPE_LTE_CMDA_EVDO_GSM_WCDMA:
-			if (!cb) {
-				rd = cbd->user;
-				parcel_init(&rilp_out);
-				parcel_w_int32(&rilp_out, 1);
-				parcel_w_int32(&rilp_out, rd->ratmode);
-				g_ril_send(rd->ril,
-					RIL_REQUEST_SET_PREFERRED_NETWORK_TYPE,
-					rilp_out.data, rilp_out.size, NULL,
-					NULL, g_free);
-				parcel_free(&rilp_out);
-			}
+			if (!cb)
+				ril_force_rat_mode(cbd->user, pref);
 			break;
 		case PREF_NET_TYPE_CDMA_EVDO_AUTO:
 		case PREF_NET_TYPE_CDMA_ONLY:
@@ -218,14 +227,12 @@ static int ril_radio_settings_probe(struct ofono_radio_settings *rs,
 	struct radio_data *rsd = g_try_new0(struct radio_data, 1);
 	rsd->ril = g_ril_clone(ril);
 	ril_get_net_config(rsd);
-	if (rsd->ratmode == PREF_NET_TYPE_GSM_WCDMA_AUTO) {
-		cbd = cb_data_new2(rsd, NULL, NULL);
-		ret = g_ril_send(rsd->ril,
-					 RIL_REQUEST_GET_PREFERRED_NETWORK_TYPE,
-					 NULL, 0, ril_rat_mode_cb, cbd, g_free);
-		if (ret <= 0)
-			g_free(cbd);
-	}
+	cbd = cb_data_new2(rsd, NULL, NULL);
+	ret = g_ril_send(rsd->ril,
+				 RIL_REQUEST_GET_PREFERRED_NETWORK_TYPE,
+				 NULL, 0, ril_rat_mode_cb, cbd, g_free);
+	if (ret <= 0)
+		g_free(cbd);
 
 	ofono_radio_settings_set_data(rs, rsd);
 	rsd->timer_id = g_timeout_add_seconds(2, ril_delayed_register, rs);
